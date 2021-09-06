25127c42dd18b35e2fc25fd1ab951a56f98efa2d
Alexandr Shilovskiy <alex.shilovskiy@gmail.com>
2021-09-07 00:27:46 +0300

    Реализован класс Hand

---
+++ lib-objects/hand.cpp
@@ -0,0 +1,44 @@
+#include "blackjack/hand.h"
+
+#include <numeric>
+
+#include "blackjack/card.h"
+
+//------------------------------------------------------------------------------
+LIB_BLACKJACK_SHARED Hand::Hand(const size_t MAXIMUM_CARD_COUNT) {
+  m_cards.reserve(MAXIMUM_CARD_COUNT);
+}
+
+//------------------------------------------------------------------------------
+LIB_BLACKJACK_SHARED Hand::~Hand() {}
+
+//------------------------------------------------------------------------------
+LIB_BLACKJACK_SHARED void Hand::add(PtrCard card) {
+  m_cards.push_back(card);
+}
+
+//------------------------------------------------------------------------------
+LIB_BLACKJACK_SHARED void Hand::clear() {
+  m_cards.clear();
+}
+
+//------------------------------------------------------------------------------
+LIB_BLACKJACK_SHARED int Hand::value() const {
+  int ace_count(0);
+  bool has_cards_with_rank(false);
+  int result(std::accumulate(
+      m_cards.begin(), m_cards.end(), 0,
+      [&ace_count, &has_cards_with_rank](int init, const PtrCard& card) -> int {
+        ace_count += static_cast<int>(card->isAce());
+        has_cards_with_rank |= card->isRankCard();
+        return init + card->value();
+      }));
+  if (has_cards_with_rank && (ace_count > 0)) {
+    /** Количество очков, добавляемое тузу при наличии старших карт
+     *  NOTE: 1 уже добавлен классом Card
+     */
+    static const int ADD_ACE_VALUE_WITH_RANK_CARDS = 10;
+    result += ace_count * ADD_ACE_VALUE_WITH_RANK_CARDS;
+  }
+  return result;
+}
--- tests/tst_card.cpp
+++ tests/tst_card.cpp
@@ -3,71 +3,72 @@
 #include "blackjack/card.h"
 
 //------------------------------------------------------------------------------
+#define TEST_CARD_IS_NOT_ACE(TYPE)   \
+  {                                  \
+    Card card(Card::DIAMONDS, TYPE); \
+    ASSERT_FALSE(card.isAce());      \
+  }
+
+TEST(Test_Card, isAce) {
+  {
+    Card card(Card::DIAMONDS, Card::ACE);
+    ASSERT_TRUE(card.isAce());
+  }
+
+  TEST_CARD_IS_NOT_ACE(Card::KING);
+  TEST_CARD_IS_NOT_ACE(Card::QUEEN);
+  TEST_CARD_IS_NOT_ACE(Card::JACK);
+  TEST_CARD_IS_NOT_ACE(Card::TEN);
+  TEST_CARD_IS_NOT_ACE(Card::NINE);
+  TEST_CARD_IS_NOT_ACE(Card::EIGHT);
+  TEST_CARD_IS_NOT_ACE(Card::SEVEN);
+  TEST_CARD_IS_NOT_ACE(Card::SIX);
+}
+
+//------------------------------------------------------------------------------
+#define TEST_CARD_IS_RANK_CARD(TYPE) \
+  {                                  \
+    Card card(Card::DIAMONDS, TYPE); \
+    ASSERT_TRUE(card.isRankCard());  \
+  }
+
+#define TEST_CARD_IS_NOT_RANK_CARD(TYPE) \
+  {                                      \
+    Card card(Card::DIAMONDS, TYPE);     \
+    ASSERT_FALSE(card.isRankCard());     \
+  }
+
+TEST(Test_Card, isRankCard) {
+  TEST_CARD_IS_RANK_CARD(Card::KING);
+  TEST_CARD_IS_RANK_CARD(Card::QUEEN);
+  TEST_CARD_IS_RANK_CARD(Card::JACK);
+  TEST_CARD_IS_NOT_RANK_CARD(Card::ACE);
+  TEST_CARD_IS_NOT_RANK_CARD(Card::TEN);
+  TEST_CARD_IS_NOT_RANK_CARD(Card::NINE);
+  TEST_CARD_IS_NOT_RANK_CARD(Card::EIGHT);
+  TEST_CARD_IS_NOT_RANK_CARD(Card::SEVEN);
+  TEST_CARD_IS_NOT_RANK_CARD(Card::SIX);
+}
+
+//------------------------------------------------------------------------------
 #define TEST_CARD_VALUE(TYPE, EXPECTED) \
-  Card card(Card::CLUBS, TYPE);         \
-  ASSERT_EQ(card.value(), EXPECTED);
+  {                                     \
+    Card card(Card::CLUBS, TYPE);       \
+    ASSERT_EQ(card.value(), EXPECTED);  \
+  }
 
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_ACE) {
+TEST(Test_Card, value) {
   TEST_CARD_VALUE(Card::ACE, 1);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_KING) {
   TEST_CARD_VALUE(Card::KING, 10);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_QUEEN) {
   TEST_CARD_VALUE(Card::QUEEN, 10);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_JACK) {
   TEST_CARD_VALUE(Card::JACK, 10);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_TEN) {
   TEST_CARD_VALUE(Card::TEN, 10);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_NINE) {
   TEST_CARD_VALUE(Card::NINE, 9);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_EIGHT) {
   TEST_CARD_VALUE(Card::EIGHT, 8);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_SEVEN) {
   TEST_CARD_VALUE(Card::SEVEN, 7);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_SIX) {
   TEST_CARD_VALUE(Card::SIX, 6);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_FIVE) {
   TEST_CARD_VALUE(Card::FIVE, 5);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_FOUR) {
   TEST_CARD_VALUE(Card::FOUR, 4);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_THREE) {
   TEST_CARD_VALUE(Card::THREE, 3);
-}
-
-//------------------------------------------------------------------------------
-TEST(Test_Card, value_TWO) {
   TEST_CARD_VALUE(Card::TWO, 2);
 }
